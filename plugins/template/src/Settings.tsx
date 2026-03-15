6dde6d9f434c0af68dcc52bc26b7fcf45c4552bd
SberKon <47828218+SberKon@users.noreply.github.com>
2026-03-15 02:23:47 -0500

    Delete plugins/template/src/Settings.tsx

--- plugins/template/src/Settings.tsx
+++
@@ -1,9 +0,0 @@
-import { Forms } from "@vendetta/ui/components";
-const { FormText } = Forms;
-
-export default () => (
-    <FormText>
-        This plugin rewrites TikTok embed URLs (vt.tiktok.com, vm.tiktok.com, www.tiktok.com, tiktok.com)
-        into fixtiktok.com for local rendering in your client. Works only locally in this client.
-    </FormText>
-)
